e9164127bc914340cab01c6efaf1d10843037dff
Mario Moreno <marioivanmorenopineda@gmail.com>
2024-09-17 21:27:38 -0400

    Correccion de rutas imagenes en los posts

--- src/posts/post1.js
+++ src/posts/post1.js
@@ -1,10 +1,12 @@
 // src/posts/post1.js
+import reactThumbnail from '../images/thumbnails/react-thumbnail.jpg';
+
 const post1 = {
-    id: 1,
-    title: 'Introducción a React',
-    content: 'React es una biblioteca de JavaScript para construir interfaces de usuario...',
-    date: '10/10/2023',
-    thumbnail: 'images/thumbnails/react-thumbnail.jpg'
-  };
-  
-  export default post1;
+  id: 1,
+  title: 'Introducción a React',
+  content: 'React es una biblioteca de JavaScript para construir interfaces de usuario...',
+  date: '10/10/2023',
+  thumbnail: reactThumbnail
+};
+
+export default post1;
--- src/posts/post2.js
+++ src/posts/post2.js
@@ -1,10 +1,12 @@
-// src/posts/post1.js
-const post1 = {
-    id: 2,
-    title: 'Desarrollo Web Moderno',
-    content: 'El desarrollo web moderno implica el uso de tecnologías como HTML5, CSS3, y JavaScript...',
-    date: '12/10/2023',
-    thumbnail: 'images/thumbnails/web-dev-thumbnail.jpg.jpg'
-  };
-  
-  export default post1;
+// src/posts/post2.js
+import webDevThumbnail from '../images/thumbnails/web-dev-thumbnail.jpg';
+
+const post2 = {
+  id: 2,
+  title: 'Desarrollo Web Moderno',
+  content: 'El desarrollo web moderno implica el uso de tecnologías como HTML5, CSS3, y JavaScript...',
+  date: '12/10/2023',
+  thumbnail: webDevThumbnail
+};
+
+export default post2;
