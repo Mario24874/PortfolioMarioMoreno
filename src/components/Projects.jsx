5c41b0744db2eb57d8035f7f11be66e605658754
Mario Moreno <marioivanmorenopineda@gmail.com>
2024-11-14 20:05:13 -0400

    Se agrega downloads y archivo APK en Projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,8 +13,8 @@ function Projects({ expandedContainer, toggleContainer }) {
         <img src={projectsIcon} alt="Projects" width="100" height="100" style={{ borderRadius: '50%' }} />
         <h2>My Projects</h2>
       </div>
-      <div className="container-content">
-        {/* Proyecto: Italianto */}
+
+      <div className="container-content">        
         <div className="project">
           <a href="https://italianto.vercel.app" target="_blank" rel="noopener noreferrer">
             <img src={logoItalianto} alt="Italianto" width="100" height="100" style={{ borderRadius: '50%' }} /> </a>
@@ -23,16 +23,30 @@ function Projects({ expandedContainer, toggleContainer }) {
               <p>Website dedicated to the teaching of the Italian language. It has many learning resources such as audios, videos, images, dictionary and translator that accompany the lessons of each course.</p>
             </div>          
         </div>
-        {/* Proyecto: Calculadora BTU */}
+        
         <div className="project">
           <a href="https://calculadorabtu.netlify.app" target="_blank" rel="noopener noreferrer">
             <img src={calculadoraBtu} alt="CalculadoraBTU" width="100" height="100" style={{ borderRadius: '50%' }} /> </a>
             <div>
               <h3>Calculator BTU</h3>            
               <p>Application to efficiently calculate the BTU needed to air-condition a space.</p>
+              <h5>Download the mobile version here:</h5>
+              <a href="/downloads/calculadoraBTU.apk" download>
+                <button style={{ 
+                  padding: '10px 15px', 
+                  backgroundColor: '#007bff', 
+                  color: '#fff', 
+                  border: 'none', 
+                  borderRadius: '5px', 
+                  cursor: 'pointer',                  
+                  marginBottom: '50px' 
+                }}>
+                  CalculadoraBTU
+                </button>
+              </a>
             </div>           
         </div>
-        {/* Proyecto: EduManager */}
+        
         <div className="project">
           <a href="https://github.com/Mario24874/edumanager-web2py" target="_blank" rel="noopener noreferrer">
             <img src={eduManager} alt="EduManager" width="100" height="100" style={{ borderRadius: '50%' }} /> </a>
@@ -41,7 +55,7 @@ function Projects({ expandedContainer, toggleContainer }) {
               <p>Desktop application for the administrative management of educational centres and institutions which performs the registration of students, scheduling of classes and classrooms as well as other events; it also stores the database and performs the issuance of reports, grades and grades of students.</p>
             </div>          
         </div>
-        {/* Proyecto: Urban Drive */}
+        
         <div className="project">
           <a href="https://urban-drives.netlify.app" target="_blank" rel="noopener noreferrer">
             <img src={urbanDrive} alt="UrbanDrive" width="100" height="100" style={{ borderRadius: '50%' }} /> </a>
